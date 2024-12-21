c9c837fb1a71afe3b3611f5475a01d1ac329144b
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2024-12-21 12:28:02 -0500

    About page creation, update next pkg

---
+++ src/app/about/page.js
@@ -0,0 +1,34 @@
+
+export default function AboutPage() {
+  return (
+    <div style={{display: 'flex', padding: '0 25vw 0 25vw', flexDirection: 'column'}}>
+      <span className='txt'>
+        After graduating college in December 2023, I began work as a software engineer the
+        following month, excited to enter the field. Amidst a year of waning job security and 
+        increased attention on the tech sector spurred by the inevitable progression of 
+        artificial intelligence models, I found myself frequently questioning legacy, 
+        long-standing practices at my company.
+        <br></br><br></br><br></br>
+        I have been accustomed to certain conventions in projects and classes during college,
+        but as I gained power and newfound goals, I allowed my inner thoughts to surface.
+        As the frequency with which you encounter a friction point increases, your tolerance
+        for this friction point and drive to resolve this friction point exponentially increase.
+        <br></br><br></br><br></br>
+        The media's incessant and obsessive reporting on artificial intelligence
+        foments an unhealthy tech sector culture: we question everything as we always knew it.
+        It is nearly impossible to navigate through a news app or site, watch television,
+        or browse the web without being bombarded by some campaign of artificial intelligence.
+        <br></br><br></br><br></br>
+        The narrative is objectively bad, but one benefit is this culture is a catalyst for
+        accelerated change. Much like the pandemic, artificial intelligence has 
+        <br></br><br></br><br></br>
+        The ubiquitous nature of technology increasingly impacts all sectors of work, and as
+        time progresses, all sectors increasingly become intertwined with the latest
+        software innovations. 
+        <br></br><br></br><br></br>
+        At <p style={{display:'inline'}} className='bz-txt'>Berzerk</p>, we embrace modern over
+        legacy, change over certainty, and purpose over __.
+      </span>
+    </div>
+  )
+}
