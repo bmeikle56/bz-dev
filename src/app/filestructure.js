3ab1bca6e2fd115d6a6566a60f6e77e588048acf
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2024-12-27 00:08:18 -0500

    Create file structure file (lol)

---
+++ src/app/filestructure.js
@@ -0,0 +1,43 @@
+
+/* do not use! */ 
+
+/// documentation and business requirements
+
+// clarify or mark source comments
+
+
+
+/// Spacing guidelines
+///   file -- 1 empty line at the top and bottom
+///   tab -- 2 spaces
+
+
+
+/// Example JS file
+/// -------------- FILE BEGINS --------------
+
+/// [1] -- random imports
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+/// [2] -- React/Motion/Next/foundation JS imports
+import { motion } from 'motion/react'
+
+/// [3] -- component barrel import
+import { Pfp } from '../components/Components'
+
+/// [4] -- make the components!
+function SomeComponent() {
+  return (
+    // the component...
+    <div/>
+  )
+}
+
+/// [5] -- export page
+export default function SomePage() {
+  return (
+    <div>
+      <SomeComponent/>
+    </div>
+  )
+}
