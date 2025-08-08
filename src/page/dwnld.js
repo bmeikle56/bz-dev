26e80845289bf1eba404907655b83f306510f68c
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-07 22:31:36 -0400

    Add file for download and add tab bar

--- src/page/dwnld.js
+++ src/page/dwnld.js
@@ -1,29 +1,31 @@
 import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolder } from '@fortawesome/free-solid-svg-icons'
+import { TabBar } from '../cmp/Components'
 
 function DownloadPage() {
   return (
     <motion.div
-    style={{
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center',
-      flexDirection: 'column',
-      width: '100vw',
-      height: '100vh',
-      background: 'black'
-    }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ delay: 0.8 }}
     >
-      <FontAwesomeIcon icon={faFolder}
-      style={{
-        color: 'rgb(238, 0, 254)',
-        width: 80,
-        height: 80,
-      }}/>
-      <a href='/files/myscript.zsh' download style={{ textDecoration: 'none' }}>
-        <pre style={{ color: 'rgb(238, 0, 254)' }}>Download</pre>
-      </a>
+      <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+        <TabBar/>
+      </div>
+      <div
+      style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', width: '100vw', height: '100vh'}}
+      >
+        <FontAwesomeIcon icon={faFolder}
+        style={{
+          color: 'rgb(238, 0, 254)',
+          width: 80,
+          height: 80,
+        }}/>
+        <a href='/files/bz.zsh' download style={{ textDecoration: 'none' }}>
+          <pre style={{ color: 'rgb(238, 0, 254)' }}>Download</pre>
+        </a>
+      </div>
     </motion.div>
   )
 }
