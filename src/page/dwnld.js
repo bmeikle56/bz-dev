7a8286e724dfdcc5ffea53b52c5cbcf00b594533
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-10 21:07:16 -0400

    Add repo struct div

--- src/page/dwnld.js
+++ src/page/dwnld.js
@@ -2,18 +2,11 @@ import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolder } from '@fortawesome/free-solid-svg-icons'
 import { TabBar } from '../cmp/Components'
+import { Spacer } from '../cmp/Components'
 
-function DownloadPage() {
+function DownloadCLI() {
   return (
-    <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ delay: 0.8 }}
-    >
-      <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
-        <TabBar/>
-      </div>
-      <div
+    <div
       style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', width: '100vw', height: '100vh'}}
       >
         <FontAwesomeIcon icon={faFolder}
@@ -26,6 +19,72 @@ function DownloadPage() {
           <pre style={{ color: 'rgb(238, 0, 254)' }}>Download</pre>
         </a>
       </div>
+  )
+}
+
+function RepoStructure() {
+
+  function Repo({ txt }) {
+    return (
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
+        <FontAwesomeIcon icon={faFolder}
+        style={{
+          color: 'rgb(238, 0, 254)',
+          width: 40,
+          height: 40,
+        }}/>
+        <pre style={{ color: 'rgb(238, 0, 254)' }}>{txt}</pre>
+      </div>
+    )
+  }
+
+  const color = 'rgb(35,35,35)'
+
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }}>
+      <Repo txt={'repo'}/>
+      <div style={{width: '1.4px', height: '8px', backgroundColor: color}}/>
+      <div style={{width: '100px', height: '1.4px', backgroundColor: color}}/>
+      <div style={{display: 'flex'}}>
+        <div style={{width: '1.4px', height: '8px', backgroundColor: color}}/>
+        <div style={{width: '1.4px', height: '8px', backgroundColor: color, marginLeft: 97}}/>
+      </div>
+      <Spacer height={10}/>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 20
+      }}>
+        <Repo txt={'berzerk-dev'}/>
+        <Repo txt={'poker-degen'}/>
+      </div>
+    </div>
+  )
+}
+
+function DownloadPage() {
+  return (
+    <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ delay: 0.8 }}
+    >
+      <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+        <TabBar/>
+      </div>
+      <DownloadCLI/>
+      <RepoStructure/>
     </motion.div>
   )
 }
