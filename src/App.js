a45dd658eba9e69079e7535ef895e3ecf91ece28
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-22 17:36:23 -0400

    Uncomment auth page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './page/home'
 import DashboardPage from './page/dash'
-//import AuthPage from './page/auth'
+import AuthPage from './page/auth'
 import DownloadPage from './page/dwnl'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<HomePage/>} />
-        {/* <Route path='/auth' element={<AuthPage/>}/> */}
+        <Route path='/auth' element={<AuthPage/>}/>
         <Route path='/dash' element={<DashboardPage/>}/>
         <Route path='/dwnl' element={<DownloadPage/>}/>
       </Routes>
